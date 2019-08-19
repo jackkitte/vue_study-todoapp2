46e6cbbfcee8e0ab836a5d26015ea7db02a62b10
jackkitte <jackkitte735@gmail.com>
2019-08-19 23:28:22 +0900

    Vuetifyの適用

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-toolbar dark color="indigo">
+      <router-link to="/">
+        <v-icon>check_circle</v-icon>
+      </router-link>
+      <v-toolbar-title class="white--text">My Todo</v-toolbar-title>
+    </v-toolbar>
+    <v-text-field v-model="newTodo" placeholder="Input here..."></v-text-field>
+    <v-btn dark color="indigo" v-on:click="addTodo()" class="white--text">ADD</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: ""
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTdo === '') return;
+      const todos = JSON.parse(localStorage.getItem('todos')) || [];
+      todos.push(this.newTdo);
+      localStorage.setItem('todos', JSON.stringify(todos));
+      this.newTdo = '';
+      this.$router.push("/");
+    }
+  }
+}
+</script>
+
+<style>
+a {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-toolbar dark color="indigo">
+      <v-toolbar-title class="white--text">My Todo</v-toolbar-title>
+    </v-toolbar>
+    <v-list>
+      <template v-for="(todo, i) in todos">
+        <v-list-tile v-bind:key="i">
+          <v-list-tile-content>
+            {{todo}}
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn text icon v-on:click="deleteTodo(i)">
+              <v-icon>delete</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-divider v-bind:key="i"></v-divider>
+      </template>
+    </v-list>
+
+    <div class="bottom-right">
+      <v-btn fab color="indigo" v-on:click="addTodo()">
+        <v-icon color="white">add</v-icon>
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: []
+    }
+  },
+  created() {
+    this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+  },
+  methods: {
+    deleteTodo(i) {
+      this.todos.splice(i, 1);
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
+    addTodo() {
+      this.$router.push("/todos/add");
+    }
+  }
+}
+</script>
+
+<style>
+.bottom-right {
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+}
+</style>
